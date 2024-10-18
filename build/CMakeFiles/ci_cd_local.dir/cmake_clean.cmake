952daef11d2b63d751cb11df2ec5a32dc3a1b7c4
alex-klem <alexey.vertol@gmail.com>
2024-10-18 18:29:06 +0300

    Add files via upload

---
+++ build/CMakeFiles/ci_cd_local.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ci_cd_local.dir/main.cpp.obj"
+  "CMakeFiles/ci_cd_local.dir/main.cpp.obj.d"
+  "ci_cd_local.exe"
+  "ci_cd_local.exe.manifest"
+  "ci_cd_local.pdb"
+  "libci_cd_local.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ci_cd_local.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ci_cd_local.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ci_cd_local.
+# This may be replaced when dependencies are built.
